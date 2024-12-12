aee9aff485d2c64d675cf0e5fe20d006f58c5f90
Saraalaaali <alaaali211178@gmail.com>
2024-12-12 22:54:26 +0200

    Memory Allocatione Panel

--- src/memalloc/MemAlloc.java
+++ src/memalloc/MemAlloc.java
@@ -4,17 +4,61 @@
  */
 package memalloc;
 
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author HANIN
  */
 public class MemAlloc {
+    static Queue<Process> StartingList = new LinkedList<Process>();
+    static LinkedList<MemBlock> MemoryBlockList = new LinkedList<>();
+    static LinkedList<Process> WaitingList = new LinkedList<Process>();
+    static LinkedList<Process> RunningList = new LinkedList<Process>();
+    static int timeOut;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Update();
+        Update();
+
     }
-    
+
+    static void Update() {
+        Iterator<Process> RunningIterator = RunningList.iterator();
+        while (RunningIterator.hasNext()) {
+            Process process = RunningIterator.next();
+            process.setTime();
+            if (process.getTime() <= 0) {
+                RunningIterator.remove();
+                freeMemory(process);
+                System.out.println("Process " + process.getID() + " ended.");
+            }
+        }
+        Iterator<Process> WaitingIterator = WaitingList.iterator();
+        while (WaitingIterator.hasNext()) {
+            Process process = WaitingIterator.next();
+            MemBlock freeBlock = findFreeBlock(process.getSize()); // Find a suitable block
+            if (freeBlock != null) {
+                allocateMemory(process, freeBlock); // Allocate memory
+                WaitingIterator.remove(); // Remove from waiting list
+                RunningList.add(process); // Move to running list
+                System.out.println("Process " + process.getID() + " moved to running list.");
+            }
+
+
+        }
+    }
+    void freeMemory(Process p){
+        MemBlock block = p.assignedBlock;
+        memoryBlocks.add(block); // Add the freed block back to the memory list
+        mergeMemoryBlocks(); // Merge adjacent free blocks (optional)
+    }
+
+    private static void initializeMemory() {
+        MemoryBlockList.add(new MemBlock(0, 1024)); // كتلة ذاكرة كاملة
+    }
+
 }
--- src/memalloc/Process.java
+++ src/memalloc/Process.java
@@ -12,19 +12,34 @@ import java.util.Random;
 
 public class Process {
     Random r=new Random();
-    private static int id = 0;
+    private static int ID = 0;
     private int size = (r.nextInt( 256)) + 1;
-    private int time = (r.nextInt( 1000)) + 1;
+    private int time = (r.nextInt( 20000)) + 1;
     private MemBlock assignedBlock;
     
     public Process(){
-        ++id;
+        ++ID;
     }
     
     void assignMemBlock(MemBlock block){
         assignedBlock = block;
     }
-    
+    public int getTime(){
+        return time;
+    }
+    public int getID(){
+        return ID;
+    }
+    public int getSize(){
+        return size ;
+    }
+    public MemBlock getAssignedBlock(){
+        return assignedBlock ;
+    }
+    public void setTime(){
+        this.time-=1000;
+    }
+
     @Override
     public String toString(){
         return "Process " + id + " with size: " + size;
